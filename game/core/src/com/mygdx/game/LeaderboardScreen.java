ab893747b78eba50ef40ce73cb9d3eb90758abb2
JV1ck3rs <jack.andrew.vickers@hotmail.co.uk>
2023-04-26 19:10:08 +0100

    Updated LeaderboardScreen.java so that the scores will be properly formatted on the screen

--- game/core/src/com/mygdx/game/LeaderboardScreen.java
+++ game/core/src/com/mygdx/game/LeaderboardScreen.java
@@ -2,8 +2,10 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -14,6 +16,13 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.mygdx.game.Core.ValueStructures.EndOfGameValues;
 
+/**
+ * This class creates and displays the leaderboard screen.
+ *
+ * @author Azzam Amirul Bahri
+ * @author Jack Vickers
+ *
+ */
 public class LeaderboardScreen implements Screen {
 
   final MyGdxGame game;
@@ -27,7 +36,7 @@ public class LeaderboardScreen implements Screen {
     scaleY = Gdx.graphics.getHeight() / 480f;
     this.game = game;
 
-    Texture leaderboardScreenTexture = new Texture(Gdx.files.internal("SemiTransparentBG.png"));
+    Texture leaderboardScreenTexture = new Texture(Gdx.files.internal("LeaderBoardBG.png"));
     Image image = new Image(leaderboardScreenTexture);
 
     // Create the stage and add the background image
@@ -41,42 +50,62 @@ public class LeaderboardScreen implements Screen {
     table.setFillParent(true);
     table.align(Align.center);
 
-    // Creates a skin for the text field using the clean-crispy-ui.json file
-    Skin skin = new Skin(Gdx.files.internal("clean-crispy-ui.json"));
+    Label score1 = new Label("1. " + "", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+    score1.setFontScale((scaleX + scaleY) / 2);
+    table.add(score1).align(Align.left).row();
+    Label score2 = new Label("2. " + "", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+    score2.setFontScale((scaleX + scaleY) / 2);
+    table.add(score2).align(Align.left).row();
+    Label score3 = new Label("3. " + "", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+    score3.setFontScale((scaleX + scaleY) / 2);
+    table.add(score3).align(Align.left).row();
+    Label score4 = new Label("4. " + "", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+    score4.setFontScale((scaleX + scaleY) / 2);
+    table.add(score4).align(Align.left).row();
+    Label score5 = new Label("5. " + "", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+    score5.setFontScale((scaleX + scaleY) / 2);
+    table.add(score5).align(Align.left).row();
+    if (numberOfCustomersServed >= 0) {
+      // Creates a skin for the text field using the clean-crispy-ui.json file
+      Skin skin = new Skin(Gdx.files.internal("clean-crispy-ui.json"));
 
-    TextField textField = new TextField(" ", skin);
-    textField.getStyle().font.getData().setScale(1.50f * (scaleX + scaleY) / 2);
-    textField.setAlignment(Align.center);
-    stage.addActor(textField); // Adds the text field to the stage
-    table.add(textField).width(250 * scaleX).height(50 * scaleY); // Adds the text field to the table
-    table.row();
+      TextField textField = new TextField(" ", skin);
+      textField.getStyle().font.getData().setScale(1.50f * (scaleX + scaleY) / 2);
+      textField.setAlignment(Align.center);
+      stage.addActor(textField); // Adds the text field to the stage
+      table.add(textField).width(250 * scaleX)
+          .height(50 * scaleY); // Adds the text field to the table
+      table.row();
 
-    TextureRegion saveScoreTexture = new TextureRegion(new Texture("SaveExitUp.png"));
-    TextureRegion saveScoreDownTexture = new TextureRegion(new Texture("SaveExitDown.png"));
-    Drawable drawableScoreBtnUp = new TextureRegionDrawable(saveScoreTexture);
-    Drawable drawableScoreBtnDown = new TextureRegionDrawable(saveScoreDownTexture);
-    Button.ButtonStyle scoreBtnStyle = new Button.ButtonStyle();
-    Button scoreBtn = new Button();
-    scoreBtn.setStyle(scoreBtnStyle);
-    scoreBtnStyle.up = drawableScoreBtnUp;
-    scoreBtnStyle.down = drawableScoreBtnDown;
-    scoreBtn.align(Align.left);
-    table.add(scoreBtn).width(250 * scaleX).height(50 * scaleY).padTop(25 * scaleY)
-        .padRight(10 * scaleY);
-    table.row();
+      TextureRegion saveScoreTexture = new TextureRegion(new Texture("SaveExitUp.png"));
+      TextureRegion saveScoreDownTexture = new TextureRegion(new Texture("SaveExitDown.png"));
+      Drawable drawableScoreBtnUp = new TextureRegionDrawable(saveScoreTexture);
+      Drawable drawableScoreBtnDown = new TextureRegionDrawable(saveScoreDownTexture);
+      Button.ButtonStyle scoreBtnStyle = new Button.ButtonStyle();
+      Button scoreBtn = new Button();
+      scoreBtn.setStyle(scoreBtnStyle);
+      scoreBtnStyle.up = drawableScoreBtnUp;
+      scoreBtnStyle.down = drawableScoreBtnDown;
+      scoreBtn.align(Align.left);
+      table.add(scoreBtn).width(250 * scaleX).height(50 * scaleY).padTop(25 * scaleY)
+          .padRight(10 * scaleY);
+      table.row();
 
-    // Adds a click listener to the easy button
-    scoreBtn.addListener(new ClickListener() {
-      @Override
-      public void clicked(InputEvent event, float x, float y) {
-        if (values.Won) {
-          LeaderboardData data = new LeaderboardData();
-          data.score = numberOfCustomersServed;
-          data.name = textField.getText();
-          game.leaderBoard.WriteHighscores(data);
+      // Adds a click listener to the easy button
+      scoreBtn.addListener(new ClickListener() {
+        @Override
+        public void clicked(InputEvent event, float x, float y) {
+          if (values.Won) {
+            LeaderboardData data = new LeaderboardData();
+            data.score = numberOfCustomersServed;
+            data.name = textField.getText();
+            game.leaderBoard.WriteHighscores(data);
+            game.setScreen(new MenuScreen(game));
+            dispose();
+          }
         }
-      }
-    });
+      });
+    }
   }
 
   /**
