4045a1ac07c33c0cc101ae4f78ceaa25d3ce8bfb
Azzam <azzamtawfiq00@gmail.com>
2023-04-26 18:32:36 +0100

    Updated LeaderboardScreen

--- game/core/src/com/mygdx/game/LeaderboardScreen.java
+++ game/core/src/com/mygdx/game/LeaderboardScreen.java
@@ -5,9 +5,15 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
+import com.mygdx.game.Core.GameState.Difficulty;
 
 public class LeaderboardScreen implements Screen {
 
@@ -44,5 +50,26 @@ public class LeaderboardScreen implements Screen {
         table.add(textField).width(250 * scaleX).height(50 * scaleY)
                 .colspan(3); // Adds the text field to the table
         table.row();
+
+        TextureRegion saveScoreTexture = new TextureRegion(new Texture("SaveExitUp.png"));
+        TextureRegion saveScoreDownTexture = new TextureRegion(new Texture("SaveExitDown.png"));
+        Drawable drawableScoreBtnUp = new TextureRegionDrawable(saveScoreTexture);
+        Drawable drawableScoreBtnDown = new TextureRegionDrawable(saveScoreDownTexture);
+        Button.ButtonStyle scoreBtnStyle = new Button.ButtonStyle();
+        Button scoreBtn = new Button();
+        scoreBtn.setStyle(scoreBtnStyle);
+        scoreBtnStyle.up = drawableScoreBtnUp;
+        scoreBtnStyle.down = drawableScoreBtnDown;
+        scoreBtn.align(Align.left);
+        table.add(scoreBtn).width(125 * scaleX).height(50 * scaleY).padBottom(25 * scaleY).padRight(10 * scaleY);
+        table.row();
+
+        // Adds a click listener to the easy button
+        scoreBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                return ;
+            }
+        });
     }
 }
